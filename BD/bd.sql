2d8d3d25acd901bd7a2e0e287e49496a15ba1b79
AngelHc <anghernandez@upt.pe>
2025-04-14 05:10:29 -0500

    actualizacion bd

--- BD/bd.sql
+++ BD/bd.sql
@@ -1,36 +1,81 @@
--- Creación de la base de datos
-CREATE DATABASE IF NOT EXISTS gestion_login;
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Versión del servidor:         10.4.32-MariaDB - mariadb.org binary distribution
+-- SO del servidor:              Win64
+-- HeidiSQL Versión:             12.10.0.7000
+-- --------------------------------------------------------
 
--- Selección de la base de datos
-USE gestion_login;
-
--- Creación de la tabla de usuarios
-CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
--- Inserción de un usuario de ejemplo
-INSERT INTO users (name, email, password) VALUES
-('Administrador', 'admin@peti.com', '$2y$10$C2VvZcZIzIl9m17a7ZLHPu2t9VwbycmC2sXOdv30md63A1T7hKm0u'); -- Contraseña: admin123
-
--- Creación de la tabla de sesiones (para el manejo de sesiones del login)
-CREATE TABLE IF NOT EXISTS sessions (
-    id VARCHAR(255) PRIMARY KEY, -- La clave primaria debe ser VARCHAR(255) para coincidir con el tipo de `id` que Laravel usa para las sesiones
-    user_id INT NOT NULL,
-    payload TEXT NOT NULL, -- Agregar la columna payload
-    last_activity TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- columna que mantiene el seguimiento de la última actividad
-    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE -- Para eliminar las sesiones relacionadas cuando un usuario sea eliminado
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 
--- Selección de la base de datos
-USE gestion_login;
+-- Volcando estructura de base de datos para gestion_login
+CREATE DATABASE IF NOT EXISTS `gestion_login` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+USE `gestion_login`;
 
--- Modificación de la tabla de sesiones para agregar las columnas faltantes
-ALTER TABLE sessions
-ADD COLUMN ip_address VARCHAR(45) NOT NULL AFTER user_id,
-ADD COLUMN user_agent TEXT NOT NULL AFTER ip_address;
+-- Volcando estructura para tabla gestion_login.cache
+CREATE TABLE IF NOT EXISTS `cache` (
+  `key` varchar(255) NOT NULL,
+  `value` mediumtext NOT NULL,
+  `expiration` int(11) NOT NULL,
+  PRIMARY KEY (`key`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Volcando datos para la tabla gestion_login.cache: ~4 rows (aproximadamente)
+INSERT INTO `cache` (`key`, `value`, `expiration`) VALUES
+	('laravel_cache_asda@adsa|127.0.0.1', 'i:1;', 1744612874),
+	('laravel_cache_asda@adsa|127.0.0.1:timer', 'i:1744612874;', 1744612874),
+	('laravel_cache_test@example.com|127.0.0.1', 'i:1;', 1744611644),
+	('laravel_cache_test@example.com|127.0.0.1:timer', 'i:1744611643;', 1744611644);
+
+-- Volcando estructura para tabla gestion_login.cache_locks
+CREATE TABLE IF NOT EXISTS `cache_locks` (
+  `key` varchar(255) NOT NULL,
+  `owner` varchar(255) NOT NULL,
+  `expiration` int(11) NOT NULL,
+  PRIMARY KEY (`key`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Volcando datos para la tabla gestion_login.cache_locks: ~0 rows (aproximadamente)
+
+-- Volcando estructura para tabla gestion_login.sessions
+CREATE TABLE IF NOT EXISTS `sessions` (
+  `id` varchar(255) NOT NULL,
+  `user_id` bigint(20) unsigned DEFAULT NULL,
+  `ip_address` varchar(45) DEFAULT NULL,
+  `user_agent` text DEFAULT NULL,
+  `payload` text NOT NULL,
+  `last_activity` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Volcando datos para la tabla gestion_login.sessions: ~2 rows (aproximadamente)
+INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, `last_activity`) VALUES
+	('ecCReqRyiEGzaOqzclrgjuKNafxNyKrofWNbeXZe', 5, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36 OPR/117.0.0.0', 'YTo0OntzOjY6Il90b2tlbiI7czo0MDoiNjFzejN2VGRuZHROVEU4cVY1cmNUR2NGek5CWmVEUzZvQlBTQWZEVyI7czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6Mjc6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9wYW5lbCI7fXM6NTA6ImxvZ2luX3dlYl81OWJhMzZhZGRjMmIyZjk0MDE1ODBmMDE0YzdmNThlYTRlMzA5ODlkIjtpOjU7fQ==', 1744625193);
+
+-- Volcando estructura para tabla gestion_login.users
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- Volcando datos para la tabla gestion_login.users: ~1 rows (aproximadamente)
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `created_at`) VALUES
+	(5, 'Administrador', 'admin@peti.com', '$2y$12$C680abLVgyqrnpTS0juqROz7Uq9Y/9PGB5eV06UTvxqagYa8k5YOS', '2025-04-14 07:49:40');
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
